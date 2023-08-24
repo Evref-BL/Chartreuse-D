bfba012670e5d1f6ed1ecda279eabb8309612c6d
Benoît Verhaeghe <benoit.verhaeghe@berger-levrault.com>
2023-08-24 16:03:28 +0200

    edit tests ressource to adapt localVariable

--- tests-res/project_b/lib/src/class_a.dart
+++ tests-res/project_b/lib/src/class_a.dart
@@ -2,7 +2,7 @@ import 'package:project_b/src/somepack/class_b.dart';
 
 class ClassA {
   void method() {
-    var i = ClassB();
-    i.methodOfB();
+    var classBLocalVariable = ClassB();
+    classBLocalVariable.methodOfB();
   }
 }
