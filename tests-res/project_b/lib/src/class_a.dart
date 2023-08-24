615b7ab81cffa3c3301651eb39129698c8618cde
Benoît Verhaeghe <benoit.verhaeghe@berger-levrault.com>
2023-08-24 16:12:44 +0200

    prepare the concept of SuperClass in test

--- tests-res/project_b/lib/src/class_a.dart
+++ tests-res/project_b/lib/src/class_a.dart
@@ -1,6 +1,7 @@
 import 'package:project_b/src/somepack/class_b.dart';
+import 'package:project_b/src/super_class_a.dart';
 
-class ClassA {
+class ClassA extends SuperClassA {
   void method() {
     var classBLocalVariable = ClassB();
     classBLocalVariable.methodOfB();
